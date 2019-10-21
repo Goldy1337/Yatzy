4df667b8d638b1ff516148e5acda046e80613d32
Goldy <vikk@live.se>
2019-10-21 10:27:15 +0200

    First commit

---
+++ src/com/company/YatzyEvaluator.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class YatzyEvaluator {
+
+    public int getRollSum(){
+        return 5; //TODO returnera rätt
+    }
+    public boolean isYatzy(){
+        return true; //TODO, returnera rätt, försök få till den snygga, kompakta variaten
+    }
+    public boolean isBigStraight(){
+        return true; //TODO, returnera rätt, försök få till den snygga, kompakta variaten
+    }
+    public boolean isSmallStraight(){
+        return true; //TODO, returnera rätt, försök få till den snygga, kompakta variaten
+    }
+    public boolean isFullHouse(){
+        return true; //TODO, returnera rätt, försök få till den snygga, kompakta variaten
+    }
+    public boolean isFourOfAKind(){
+        return true; //TODO, returnera rätt, försök få till den snygga, kompakta variaten
+    }
+    public boolean isThreeOfAKind(){
+        return true; //TODO, returnera rätt, försök få till den snygga, kompakta variaten
+    }
+    public boolean isTwoOfAKind(){
+        return true; //TODO, returnera rätt, försök få till den snygga, kompakta variaten
+    }
+
+}
